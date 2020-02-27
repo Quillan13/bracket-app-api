a9f1609134302b7eed434807026bc6de1a83bcf4
Quillan Ogle <quillan.ogle@ruralsource.com>
2020-02-27 14:35:38 -0500

    added constructor

--- Models/UserSettings/UserSettings.cs
+++ Models/UserSettings/UserSettings.cs
@@ -2,6 +2,12 @@ namespace BracketApp.Api.Models.UserSettings
 {
     public class UserSettings : Document
     {
+        public UserSettings(CreateUserSettingsViewModel viewModel, string ownerId) : base(ownerId)
+        {
+            Primary = viewModel.Primary;
+            Secondary = viewModel.Secondary;
+            Tertiary = viewModel.Tertiary;
+        }
         public string Primary { get; set; }
 
         public string Secondary { get; set; }
